4add4d5970e5e18a1ac77d50c6d6a480e76fe94a
Javontay McElroy <dinomightnetwork@gmail.com>
2019-03-11 12:46:48 -0500

    Mapped through the data. Only have the names tho.

--- src/App.js
+++ src/App.js
@@ -6,13 +6,32 @@ import Dashboard from './components/Dashboard';
 import './App.css';
 import Nav from './components/Nav';
 import Home from './components/Home';
+import axios from 'axios';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      pokemon: []
+    };
+  }
+
+  componentDidMount() {
+    axios
+      .get('http://localhost:8000/api/pokemon')
+      .then(res => this.setState({ pokemon: res.data }))
+      .catch(err => console.log(err));
+  }
+
   render() {
     return (
       <>
         <Route component={Nav} />
-        <Route exact path='/' component={Home} />
+        <Route
+          exact
+          path='/'
+          render={props => <Home {...props} pokemon={this.state.pokemon} />}
+        />
         <Route component={Dashboard} path='/dashboard' />
         <div className='bg-elements'>
           <span className='sidebar-left' />
--- src/components/Home.js
+++ src/components/Home.js
@@ -4,18 +4,14 @@ import Search from './Search';
 
 import './Home.css';
 
-class Home extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
-  render() {
-    return (
-      <>
-        <Search />
-        <div className='home-container'>
+const Home = ({ pokemon }) => {
+  return (
+    <>
+      <Search />
+      <div className='home-container'>
+        {pokemon.map(poke => (
           <div className='pokemon'>
-            <h1 className='pokemon-name'>Pokemon</h1>
+            <h1 className='pokemon-name'>{poke.name}</h1>
             <span className='poke-frame' />
             <img
               src='https://i.ibb.co/k53vsg2/Pikachu.gif'
@@ -23,136 +19,10 @@ class Home extends Component {
               className='home-pokemon'
             />
           </div>
-          {/* <div className='pokemon'>
-            <h1 className='pokemon-name'>Pokemon</h1>
-            <span className='poke-frame' />
-            <img
-              src='https://i.ibb.co/PTg7DZJ/Abra.gif'
-              alt='pokemon'
-              className='home-pokemon'
-            />
-          </div>
-          <div className='pokemon'>
-            <h1 className='pokemon-name'>Pokemon</h1>
-            <span className='poke-frame' />
-            <img
-              src='https://i.ibb.co/gWdzrFb/Blastoise.gif'
-              alt='pokemon'
-              className='home-pokemon'
-            />
-          </div>
-          <div className='pokemon'>
-            <h1 className='pokemon-name'>Pokemon</h1>
-            <span className='poke-frame' />
-            <img
-              src='https://i.ibb.co/qgXGGRK/Bulbasaur.gif'
-              alt='pokemon'
-              className='home-pokemon'
-            />
-          </div>
-          <div className='pokemon'>
-            <h1 className='pokemon-name'>Pokemon</h1>
-            <span className='poke-frame' />
-            <img
-              src='https://i.ibb.co/QJp7sYb/Butterfree-Female.gif'
-              alt='pokemon'
-              className='home-pokemon'
-            />
-          </div>
-          <div className='pokemon'>
-            <h1 className='pokemon-name'>Pokemon</h1>
-            <span className='poke-frame' />
-            <img
-              src='https://i.ibb.co/n1WkDw9/Charizard.gif'
-              alt='pokemon'
-              className='home-pokemon'
-            />
-          </div>
-          <div className='pokemon'>
-            <h1 className='pokemon-name'>Pokemon</h1>
-            <span className='poke-frame' />
-            <img
-              src='https://i.ibb.co/hm6vKdd/Haunter.gif'
-              alt='pokemon'
-              className='home-pokemon'
-            />
-          </div>
-          <div className='pokemon'>
-            <h1 className='pokemon-name'>Pokemon</h1>
-            <span className='poke-frame' />
-            <img
-              src='https://i.ibb.co/F4XQwFZ/Mr-Mime.gif'
-              alt='pokemon'
-              className='home-pokemon'
-            />
-          </div>
-          <div className='pokemon'>
-            <h1 className='pokemon-name'>Pokemon</h1>
-            <span className='poke-frame' />
-            <img
-              src='https://i.ibb.co/YLTDhmj/Mew.gif'
-              alt='pokemon'
-              className='home-pokemon'
-            />
-          </div>
-          <div className='pokemon'>
-            <h1 className='pokemon-name'>Pokemon</h1>
-            <span className='poke-frame' />
-            <img
-              src='https://i.ibb.co/9h1k9yv/Gyarados-Female.gif'
-              alt='pokemon'
-              className='home-pokemon'
-            />
-          </div>
-          <div className='pokemon'>
-            <h1 className='pokemon-name'>Pokemon</h1>
-            <span className='poke-frame' />
-            <img
-              src='https://i.ibb.co/QrW7FXy/Golbat-Female.gif'
-              alt='pokemon'
-              className='home-pokemon'
-            />
-          </div>
-          <div className='pokemon'>
-            <h1 className='pokemon-name'>Pokemon</h1>
-            <span className='poke-frame' />
-            <img
-              src='https://i.ibb.co/F70YhG2/Jigglypuff.gif'
-              alt='pokemon'
-              className='home-pokemon'
-            />
-          </div>
-          <div className='pokemon'>
-            <h1 className='pokemon-name'>Pokemon</h1>
-            <span className='poke-frame' />
-            <img
-              src='https://i.ibb.co/CKS4X4z/Rapidash.gif'
-              alt='pokemon'
-              className='home-pokemon'
-            />
-          </div>
-          <div className='pokemon'>
-            <h1 className='pokemon-name'>Pokemon</h1>
-            <span className='poke-frame' />
-            <img
-              src='https://i.ibb.co/SP69YDX/Rayquaza-Mega.gif'
-              alt='pokemon'
-              className='home-pokemon'
-            />
-          </div>
-          <div className='pokemon'>
-            <h1 className='pokemon-name'>Pokemon</h1>
-            <span className='poke-frame' />
-            <img
-              src='https://i.ibb.co/njj850C/Vibrava.gif'
-              alt='pokemon'
-              className='home-pokemon'
-            />
-          </div> */}
-        </div>
-      </>
-    );
-  }
-}
+        ))}
+      </div>
+    </>
+  );
+};
 
 export default Home;

f9b2d2ea3cbe959a44def4d72e7f10d51ae9c024
Brandon L. Borrero <borrero509@outlook.com>
2019-03-11 14:17:10 -0700

    set up register and login

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Register from "./Register";
+import Login from "./Login";
+import { Route, Link } from 'react-router-dom';
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+       <Link to="/login">Login Here</Link>
+       <Link to="/register">Register Here</Link>
+       <Route exact path="/login" component={Login}/>
+       <Route exact path="/register" component={Register}/>  
       </div>
     );
   }

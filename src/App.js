187d5c9b12c03ff9cd2fb1b3d41307f5ec75b074
wisdomtooth <cbtsao47@gmail.com>
2019-03-12 10:45:13 -0500

    fix cdm

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import React, { Component } from 'react';
-import Register from './Register';
-import Login from './Login';
-import { Route, Link } from 'react-router-dom';
-import Dashboard from './components/Dashboard';
-import Nav from './components/Nav';
-import Home from './components/Home';
-import axios from 'axios';
-import './App.css';
+import React, { Component } from "react";
+import Register from "./Register";
+import Login from "./Login";
+import { Route, Link, withRouter } from "react-router-dom";
+import Dashboard from "./components/Dashboard";
+import Nav from "./components/Nav";
+import Home from "./components/Home";
+import axios from "axios";
+import "./App.css";
 
 class App extends Component {
   constructor() {
@@ -18,7 +18,7 @@ class App extends Component {
   }
 
   componentDidUpdate() {
-    if (localStorage.getItem('token')) {
+    if (localStorage.getItem("token")) {
       axios
         .get(
           `https://pokepokepokedex.herokuapp.com/api/pokemon?page=${
@@ -26,20 +26,18 @@ class App extends Component {
           }`,
           {
             headers: {
-              'Content-Type': 'application/json',
+              "Content-Type": "application/json",
               Authorization: window.localStorage.token
             }
           }
         )
         .then(res => this.setState({ pokemon: res.data.data }))
         .catch(err => console.log(err));
-    } else {
-      // this.props.history.push('/login');
     }
   }
 
   componentDidMount() {
-    if (localStorage.getItem('token')) {
+    if (localStorage.getItem("token")) {
       axios
         .get(
           `https://pokepokepokedex.herokuapp.com/api/pokemon?page=${
@@ -47,7 +45,7 @@ class App extends Component {
           }`,
           {
             headers: {
-              'Content-Type': 'application/json',
+              "Content-Type": "application/json",
               Authorization: window.localStorage.token
             }
           }
@@ -55,17 +53,17 @@ class App extends Component {
         .then(res => this.setState({ pokemon: res.data.data }))
         .catch(err => console.log(err));
     } else {
-      // this.props.history.push('/login');
+      this.props.history.push("/login");
     }
   }
 
   pageChange = event => {
     // debugger;
-    if (event.target.name === 'prev' && this.state.pageNumber === 1) {
+    if (event.target.name === "prev" && this.state.pageNumber === 1) {
       return;
     }
 
-    if (event.target.name === 'next') {
+    if (event.target.name === "next") {
       this.setState({ pageNumber: this.state.pageNumber + 1 });
     } else {
       this.setState({ pageNumber: this.state.pageNumber - 1 });
@@ -78,7 +76,7 @@ class App extends Component {
         <Route component={Nav} />
         <Route
           exact
-          path='/home'
+          path="/home"
           render={props => (
             <Home
               {...props}
@@ -88,24 +86,24 @@ class App extends Component {
           )}
         />
         <Route
-          path='/dashboard/:id'
+          path="/dashboard/:id"
           render={props => (
             <Dashboard {...props} pokemon={this.state.pokemon} />
           )}
         />
-        <Route exact path='/login' component={Login} />
-        <Route exact path='/register' component={Register} />
-        <div className='bg-elements'>
-          <span className='sidebar-left' />
-          <span className='sidebar-right' />
-          <span className='bar-bottom' />
-          <span className='dotted-grid' />
-          <span className='bg-image' />
-          <span className='blur' />
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/register" component={Register} />
+        <div className="bg-elements">
+          <span className="sidebar-left" />
+          <span className="sidebar-right" />
+          <span className="bar-bottom" />
+          <span className="dotted-grid" />
+          <span className="bg-image" />
+          <span className="blur" />
         </div>
       </>
     );
   }
 }
 
-export default App;
+export default withRouter(App);

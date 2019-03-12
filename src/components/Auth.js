f55bebf548f0bffebb64654f91b324dca5693f0c
wisdomtooth <cbtsao47@gmail.com>
2019-03-12 11:30:03 -0500

    fix login

---
+++ src/components/Auth.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Auth(Login) {
+  if (localStorage.getItem("token")) return <div />;
+}
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,19 +1,19 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-class Searcg extends Component {
-    constructor(props) {
-      super(props);
-      this.state = {};
-    }
-    render() {
-      return (
-        <div className='search-container'>
-        <form>
-            <input placeholder='search' type='text' name='search' />
-        </form>
-        </div>
-      );
-    }
+class Search extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
   }
-  
-  export default Searcg;
+  render() {
+    return (
+      <div className="search-container">
+        <form>
+          <input placeholder="search" type="text" name="search" />
+        </form>
+      </div>
+    );
+  }
+}
+
+export default Search;

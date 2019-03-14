4727113383b02347f67c8387bdc283f63fc1f1ba
wisdomtooth <cbtsao47@gmail.com>
2019-03-14 15:04:21 -0500

    fix login reroute

--- src/Login.js
+++ src/Login.js
@@ -1,13 +1,13 @@
-import React, { Component } from 'react';
-import axios from 'axios';
+import React, { Component } from "react";
+import axios from "axios";
 
-import './Login.css';
+import "./Login.css";
 
 export default class Login extends Component {
   state = {
     loginInfo: {
-      username: '',
-      password: ''
+      username: "",
+      password: ""
     }
   };
 
@@ -23,67 +23,68 @@ export default class Login extends Component {
   tryLogin = async e => {
     e.preventDefault();
     try {
-      return await axios
+      const result = await axios
         .post(
-          'https://pokepokepokedex.herokuapp.com/auth/login',
+          "https://pokepokepokedex.herokuapp.com/auth/login",
           this.state.loginInfo,
           {
             headers: {
-              'Content-Type': 'application/json',
+              "Content-Type": "application/json",
               Authorization: window.localStorage.token
             }
           }
         )
         .then(res => {
           console.log(res);
-          localStorage.setItem('token', res.data.token);
-          localStorage.setItem('id', res.data.id);
+          localStorage.setItem("token", res.data.token);
+          localStorage.setItem("id", res.data.id);
           this.setState({
             registerInfo: {
-              username: '',
-              password: ''
+              username: "",
+              password: ""
             }
           });
-          window.location.href = '/home';
         });
+      this.props.history.push("/home");
+      return result;
     } catch (error) {
       console.log(error);
     }
   };
 
   takeToRegister = () => {
-    this.props.history.push('/register');
+    this.props.history.push("/register");
   };
 
   render() {
     return (
       <div>
-        <div className='login-text'>
+        <div className="login-text">
           <img
             src={require(`../src/assets/pokestat-login.png`)}
-            className='login-header'
-            alt='logo'
+            className="login-header"
+            alt="logo"
           />
         </div>
-        <form onSubmit={this.tryLogin} className='login-container'>
+        <form onSubmit={this.tryLogin} className="login-container">
           <input
-            type='text'
-            name='username'
+            type="text"
+            name="username"
             value={this.state.loginInfo.username}
             onChange={this.handleChanges}
-            placeholder='username'
+            placeholder="username"
           />
           <input
-            type='password'
-            name='password'
+            type="password"
+            name="password"
             value={this.state.loginInfo.password}
             onChange={this.handleChanges}
-            placeholder='password'
+            placeholder="password"
           />
-          <button onClick={this.tryLogin} className='log-in-btn'>
+          <button onClick={this.tryLogin} className="log-in-btn">
             Log in
           </button>
-          <button onClick={this.takeToRegister} className='register-btn'>
+          <button onClick={this.takeToRegister} className="register-btn">
             Need an account?
           </button>
         </form>
